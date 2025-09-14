4c508438f3ce7d2c2d52dfa3a7b049488600eabf
abd_alrahman <anonymospesrson@gmail.com>
2025-09-15 01:26:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const { writeFile, readFile, unlink } = require(`fs`);
 const multer = require(`multer`);
 const methodOverride = require('method-override');
 const sql = require(`mysql2`);
+const dotenv = require(`dotenv`);
+
+dotenv.config();
 
 const db = sql.createConnection({
-    host:`localhost`,
-    user:`root`,
-    password:`AK19701970`,
-    database:`resturantstats`
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
 
 db.connect((err)=>{
